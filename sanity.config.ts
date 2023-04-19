b0d0598b7b207cc4e73dcd9ae307815e8d6fd286
vlaier <piotrzielu.1996@gmail.com>
2023-04-19 13:05:05 +0200

    fix: fix bug that involves client configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,14 +7,11 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID;
 if (!projectId) {
   throw new Error('Missing NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID');
 }
-export const baseConfig = {
+const config = defineConfig({
   projectId,
   dataset: 'production',
   title: 'Keeping Sanity',
   apiVersion: '2023-04-18',
-};
-const config = defineConfig({
-  ...baseConfig,
   basePath: '/admin',
   plugins: [deskTool()],
   schema: { types: schemas },
--- sanity/utils.ts
+++ sanity/utils.ts
@@ -1,6 +1,12 @@
 import { Project } from '@/types/Project';
 import { createClient, groq } from 'next-sanity';
-import { baseConfig as config } from '@/sanity.config';
+const projectId = process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID;
+const config = {
+  projectId,
+  dataset: 'production',
+  title: 'Keeping Sanity',
+  apiVersion: '2023-04-18',
+};
 const client = createClient(config);
 export async function getProjects(): Promise<Project[]> {
   return client.fetch(groq`*[_type == "project"]{
