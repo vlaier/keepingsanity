823e3272a13c691346bb3e3149fef12f7fd51ced
vlaier <piotrzielu.1996@gmail.com>
2023-04-19 14:07:01 +0200

    style: add basic styling to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,44 @@
 import { getProjects } from '@/sanity/utils';
+import Image from 'next/image';
 export default async function Home() {
   const projects = await getProjects();
-  const projectElements = projects.map((project) => (
-    <div key={project._id} className="bg-blue-200 w-24 h-24">
+  const projectsElement = projects.map((project) => (
+    <div
+      key={project._id}
+      className="rounded-lg border border-gray-500 flex w-fit"
+    >
       <h2>{project.name}</h2>
+      <Image
+        src={project.image}
+        alt={project.name}
+        width={250}
+        height={250}
+        className="object-fit"
+      />
     </div>
   ));
-  return <div className="bg-red-500 w-96 h-96">{projectElements}</div>;
+  return (
+    <div className="max-w-5xl mx-auto">
+      <h1 className="text-7xl font-extrabold py-20">
+        Hello{' '}
+        <span className="bg-gradient-to-r from-orange-400 to-purple-600 bg-clip-text text-transparent">
+          World!
+        </span>
+      </h1>
+      <div>
+        <h2>About me</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+          quibusdam voluptatem tempora accusantium vitae ullam aliquid tempore
+          eveniet libero voluptatibus.
+        </p>
+      </div>
+      <div>
+        <h2>My Projects</h2>
+        <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:gird-cols-3 xl:gird-cols-4">
+          {projectsElement}
+        </div>
+      </div>
+    </div>
+  );
 }
