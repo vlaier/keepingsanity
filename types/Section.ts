e2c2803d92ee2bad80e3f97efc74f98e13cfa9cd
vlaier <piotrzielu.1996@gmail.com>
2023-04-20 14:55:03 +0200

    feat: add section schema and corresponding types

--- types/Section.ts
+++ types/Section.ts
@@ -1,11 +1,9 @@
 import { PortableTextBlock } from 'sanity';
 
-export type ProjectType = {
+export type SectionType = {
   _id: string;
   _createdAt: Date;
-  name: string;
+  title: string;
   slug: string;
-  image: string;
-  url: string;
   content: PortableTextBlock[];
 };
