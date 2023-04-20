2371787a8f7049efdee6222400838f1a3cc3dcc9
vlaier <piotrzielu.1996@gmail.com>
2023-04-20 15:02:18 +0200

    feat: add Badge component

---
+++ components/ui/Badge.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Badge = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <li className="text-xs font-medium px-2.5 py-0.5 rounded-full bg-gray-700 text-gray-300">
+      {children}
+    </li>
+  );
+};
+
+export default Badge;
