062bc0eb2de15fb2d56a5bda8a43fc1ff6ca09b0
vlaier <piotrzielu.1996@gmail.com>
2023-04-25 12:12:04 +0200

    feat: add blog schema

---
+++ sanity/schemas/blog.ts
@@ -0,0 +1,43 @@
+const blog = {
+  name: 'blog',
+  title: 'Blogs',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          title: 'Alt',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{ type: 'block' }],
+    },
+  ],
+};
+export default blog;
--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,4 +1,5 @@
+import blog from './blog';
 import project from './project';
 import section from './section';
 import social from './social';
-export default [project, section, social];
+export default [project, section, social, blog];
